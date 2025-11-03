9c088e48c92af3465a001d723f2c1b08ad27e6ac
jonod5 <jonathandau09@gmail.com>
2025-11-03 02:54:50 -0800

    Local save feature

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,29 +6,26 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const [text, setText] = useState(localStorage.getItem('savedText') || '');
+
+  const handleSave = () => {
+    localStorage.setItem('savedText', text);
+  };
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <button className="saveButton" onClick={handleSave}>Save</button>
+  <div>
+    <textarea className="textBox" id="body" placeholder="Write here.."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        />
+        <br />
+  </div>
+    
+    
     </>
+      
   )
 }
 
