d168199899f7cef7fb1279bad6bb8a785b4d7e7b
Dabbuka <luca.tineo01@gmail.com>
2025-11-22 02:44:55 -0800

    Added react-router-dom package for navigating pages in the app, and refactored App.jsx to act as the router between different webpages. Note editor logic moved to new pages directory that contains structure of each webpage.

---
+++ frontend/src/pages/NoteEditor.jsx
@@ -0,0 +1,93 @@
+import React, { useState, useRef, useEffect } from 'react'
+import axios from 'axios'
+import MDEditor, { commands, heading1, heading2, heading3, heading4, heading5, heading6 } from '@uiw/react-md-editor';
+import '../App.css'
+import '@uiw/react-md-editor/markdown-editor.css';
+import '@uiw/react-markdown-preview/markdown.css';
+
+
+function App() {
+  const noteId = "6917d095d3794db386c18f88" // ID for note titled Test #3
+  const [value, setValue] = useState("");
+
+  // Fetch note data on mount
+  useEffect(() => {
+      async function fetchNote() {
+        try {
+          const res = await axios.get(`/api/notes/${noteId}`);
+          console.log(res.data);
+          setValue(res.data.content);
+        } catch (err) {
+          console.error(err);
+        }
+      }
+
+      fetchNote();
+  }, [noteId]);
+
+  const handleSave = () => {
+    localStorage.setItem('savedText', value);
+  };
+
+  /* This is to put the save button in the toolbar */
+  const saveCommand = {
+    name:"save",
+    keyCommand:"save",
+    icon: <button className="save-button" onClick={handleSave}> Save Document </button>,
+    execute: (state, api) => {
+      localStorage.setItem('savedText', state.text);
+    }
+    
+  
+  }
+  
+  
+
+  return (
+    <>
+    
+      <div className="custom-md-editor">
+        
+        <MDEditor
+          value={value}
+          onChange={setValue}
+          visibleDragbar = {false}
+          
+          commands = {[
+            saveCommand,
+            commands.group([
+              commands.title1,
+              commands.title2,
+              commands.title3,
+              commands.title4,
+              commands.title5,
+              commands.title6
+            ], {
+              name: 'title',
+              groupName: 'title',
+              buttonProps: { 'aria-label': 'Insert title' },
+              className:"title-button",
+            }),       // All heading levels grouped
+            commands.bold,         // Bold **
+            commands.italic,       // Italics
+            commands.strikethrough,
+            commands.hr,           // Horizontal Rule ---
+            commands.link,         // Insert link
+            commands.image,       // Insert image
+            commands.unorderedListCommand, // Bullet list
+            commands.orderedListCommand,   // Numbered list
+            commands.code,         // Inline code
+            commands.table,
+            commands.divider,      // Toolbar divider line
+            
+          ]}
+        />
+        
+      </div>
+
+
+    </>
+  );
+}
+
+export default App;
