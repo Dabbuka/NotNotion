1753d27116934e20fe7b457e93ce192c7224cdef
Dabbuka <luca.tineo01@gmail.com>
2025-11-28 21:35:20 -0800

    Refactored the landing page and separated the css into a new file.

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import './css/LandingPage.css'
 const LandingPage = () => {
   return (
-    <div style={{ textAlign: 'center', marginTop: '100px', color: 'white' }}>
+    <div className="landing-page">
       <h1>Welcome to NotNotion</h1>
       <p>To be stylized later</p>
       
-      <div style={{ display: 'flex', justifyContent: 'center', gap: '20px', marginTop: '40px' }}>
-        {/* These Links swap the component without reloading the page */}
+      <div className="clickable-buttons">
+        {/* Change to a different page without reloading, more efficient than an href */}
         <Link to="/login">
-          <button style={{backgroundColor: '#646'}}>Login</button>
+          <button className="redirect-buttons">Login</button>
         </Link>
         
         <Link to="/register">
-          <button style={{backgroundColor: '#646'}}>Register</button>
+          <button className="redirect-buttons">Register</button>
         </Link>
         
         <Link to="/app">
-          <button style={{ backgroundColor: '#646' }}>Go to Notes (Demo)</button>
+          <button className="redirect-buttons">Go to Notes</button>
         </Link>
       </div>
     </div>
