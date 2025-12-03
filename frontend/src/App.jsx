cbfe01db0c2d14acfeddfcf368e8236fb807f161
ipark15 <isiahpark@gmail.com>
2025-12-03 01:19:03 -0800

    added navbar and simplified landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Navbar from './pages/Navbar';
 import LandingPage from './pages/LandingPage';
 import NoteEditor from './pages/NoteEditor';
 import Register from './pages/Register';
@@ -8,18 +9,17 @@ import Login from './pages/Login';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* / URL shows landing page */}
-        <Route path="/" element={<LandingPage />} />
-        {/* /app URL shows note editor*/}
-        <Route path="/app" element={<NoteEditor />} /> 
-        {/* /login shows login screen */}
-        <Route path="/login" element={<Login />} />
-        {/* /register shows registration screen */}
-        <Route path="/register" element={<Register />} /> 
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        {/* Navbar appears on all pages */}
+        <Navbar />
+        
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/app" element={<NoteEditor />} /> 
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} /> 
+        </Routes>
+      </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import './css/Navbar.css';
+
+function Navbar() {
+  const location = useLocation();
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <Link to="/" className="navbar-logo">
+          NotNotion
+        </Link>
+      </div>
+      
+      <div className="navbar-links">
+        <Link 
+          to="/" 
+          className={`navbar-link ${location.pathname === '/' ? 'active' : ''}`}
+        >
+          Home
+        </Link>
+        <Link 
+          to="/login" 
+          className={`navbar-link ${location.pathname === '/login' ? 'active' : ''}`}
+        >
+          Login
+        </Link>
+        <Link 
+          to="/register" 
+          className={`navbar-link navbar-link-register ${location.pathname === '/register' ? 'active' : ''}`}
+        >
+          Register
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
