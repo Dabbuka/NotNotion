8d5fbadbced5b9027500ce86390f1ae1d6d0336a
Dabbuka <luca.tineo01@gmail.com>
2025-12-03 01:26:52 -0800

    feat: Added backend user authentication (registration/login) with JWT and bcrypt.

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import noteRoutes from "./routes/noteRoutes.js";
 import folderRoutes from "./routes/folderRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 
@@ -12,6 +13,6 @@ app.use(express.json());
 // Routes
 app.use("/api/notes", noteRoutes);
 app.use("/api/folders", folderRoutes);
-
+app.use("/api/users", userRoutes);
 
 export default app;
---
+++ backend/src/controllers/folderController.js
@@ -0,0 +1,25 @@
+import folder from "../models/folder.js";
+
+/**
+ * @route POST /api/folders
+ * @desc Create a folder (stub for rn)
+ */
+export const createFolder = async (req, res) => {
+    res.status(501).json({ message: "Folder creation to be implemented later"});
+};
+
+/**
+ * @route GET /api/folders
+ * @desc Get folders (stub for rn)
+ */
+export const getFolder = async (req, res) => {
+    res.status(501).json({ message: "Folder retrieval to be implemented later"});
+};
+
+/**
+ * @route PUT /api/folders
+ * @desc Rename folder (stub for rn)
+ */
+export const renameFolder = async (req, res) => {
+    res.status(501).json({ message: "Folder renaming to be implemented later"});
+};
