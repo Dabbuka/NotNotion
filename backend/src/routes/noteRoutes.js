6d041996d3e59ddd24454b567af86444f48f0da4
kouseph <kouseph@g.ucla.edu>
2025-12-04 04:10:07 -0800

    cleaned up noteRoute comments

--- backend/src/routes/noteRoutes.js
+++ backend/src/routes/noteRoutes.js
@@ -3,11 +3,8 @@ import { createNote, getNotes, updateNote } from "../controllers/noteController.
 
 const router = express.Router();
 
-// POST/GET methods from /api/notes
-// '/' is used instead of '/api/notes' is because prefix is handled in app.js
 router.post("/createNote", createNote);
 router.get("/:id", getNotes);
 router.patch("/:id", updateNote);
-// router.post("/:id, updateNote") -- TODO
 
 export default router;
