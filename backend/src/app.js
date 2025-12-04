b6f8eaf7afa2c3110f2f2f58928f6463e2768599
Dabbuka <luca.tineo01@gmail.com>
2025-12-04 00:19:25 -0800

    Refactored app.js to only import folderRoutes instead of containing folderRoute functions inside of it.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import { createFolder, getFolder, renameFolder, addFolder } from "./controllers/folderController.js";
 import noteRoutes from "./routes/noteRoutes.js";
 import folderRoutes from "./routes/folderRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -18,11 +17,5 @@ app.get('/', (req, res) => {
 app.use("/api/notes", noteRoutes);
 app.use("/api/folders", folderRoutes);
 app.use("/api/users", userRoutes);
-
-//TODO: rename the routes to be more appropriate once connected to frontend and backend
-app.post("/createFolder", createFolder);
-app.get("/renameFolder", renameFolder);
-app.get("/:id", getFolder);
-app.patch("/renameFolder", addFolder);
 
 export default app;
